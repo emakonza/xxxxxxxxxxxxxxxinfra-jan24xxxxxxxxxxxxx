46141667b8767139f1d9a0447fe83c7b8e61b5f6
emakonza <147326124+emakonza@users.noreply.github.com>
2024-02-17 16:26:26 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "karo-terraform-state"
     key    = "global/s3/terraform.tfstate"
     region = "eu-west-2"
-    dynamo_table = "terraform-state"
+    dynamoDB_table = "terraform-state"
     encrypt = true
   }
 }
