960a00ace0af93704b86d5e975b6284fd3fd9c1d
emakonza <147326124+emakonza@users.noreply.github.com>
2024-02-17 22:43:24 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "karo-terraform-state"
-#    key    = "global/s3/terraform.tfstate"
-#    region = "eu-west-2"
-#    dynamodb_table = "terraform-state"
-#    encrypt = true
-#  }
-#}
-#
+terraform {
+  backend "s3" {
+    bucket = "karo-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "terraform-state"
+    encrypt = true
+  }
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
