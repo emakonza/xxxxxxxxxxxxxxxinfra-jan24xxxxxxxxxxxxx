aeb402aa17ba5b3ba29f635195747e0dfa240ff2
emakonza <147326124+emakonza@users.noreply.github.com>
2024-02-19 21:40:25 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "karo-terraform-state"
     key    = "global/s3/terraform.tfstate"
-    region = "eu-west-2"
+    region = "us-east-1"
     dynamodb_table = "terraform-state"
     encrypt = true
   }
